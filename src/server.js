eec8bd8cfab8ee8a02cbce6148b952a4d52dc512
Carlos Henrique Nunes Correa <carloscorrea.dpss@gmail.com>
2024-01-17 17:05:58 -0300

    Added get method to the Server

--- src/server.js
+++ src/server.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
-const PORT = 3333;
+app.get("/message/:id/:user", (request, response) => {
+  const { id, user } = request.params;
+  response.send(`Message ID: ${id}. For User ${user}.`)
+});
 
+const PORT = 3333;
 app.listen(PORT,() => console.log(`server is running on Port ${PORT}`));
