0e0ebdd900d77e652585e9965175a3536f1b2287
Carlos Henrique Nunes Correa <carloscorrea.dpss@gmail.com>
2024-01-18 09:03:53 -0300

    Installed and configured the Nodemon package.

--- src/server.js
+++ src/server.js
@@ -7,5 +7,12 @@ app.get("/message/:id/:user", (request, response) => {
   response.send(`Message ID: ${id}. For User ${user}.`)
 });
 
+
+app.get("/users", (request, response) => {
+const {page, limit } = request.query
+response.send(`Page: ${page}. Show ${limit}`)
+});
+
+
 const PORT = 3333;
 app.listen(PORT,() => console.log(`server is running on Port ${PORT}`));
